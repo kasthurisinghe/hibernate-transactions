e0387952ad7fdd11c3dc11aa23fc8b22e4182657
Punsara Kasthurisinghe <kasthurisinghekdpk@gmail.com>
2023-09-10 22:56:25 +0530

    Item table and new Orderdetail table

--- src/util/SessionFactoryConfiguration.java
+++ src/util/SessionFactoryConfiguration.java
@@ -5,6 +5,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import entity.CustomerEntity;
+import entity.ItemEntity;
+import entity.OrderDetailEntity;
 import entity.OrderEntity;
 
 public class SessionFactoryConfiguration {
@@ -15,7 +17,10 @@ public class SessionFactoryConfiguration {
     private SessionFactoryConfiguration() {
         Configuration configuration = new Configuration().configure()
         .addAnnotatedClass(CustomerEntity.class)
-        .addAnnotatedClass(OrderEntity.class);
+        .addAnnotatedClass(OrderEntity.class)
+        .addAnnotatedClass(OrderDetailEntity.class)
+        .addAnnotatedClass(ItemEntity.class)
+        .addAnnotatedClass(OrderDetailEntity.class);
 
         sessionFactory = configuration.buildSessionFactory();
     }
